1a771f07a19c3d4bd4f2d7a5ec5e8d53341b2937
Elmannaniayoub <Ayoubelmannnani3@gmail.com>
2020-04-23 20:39:34 +0200

    Event Component

--- src/app/home/home.module.ts
+++ src/app/home/home.module.ts
@@ -5,6 +5,8 @@ import { FormsModule } from '@angular/forms';
 import { RouterModule } from '@angular/router';
 
 import { HomePage } from './home.page';
+import { EventComponent } from '../event/event.component';
+
 
 @NgModule({
   imports: [
@@ -18,6 +20,6 @@ import { HomePage } from './home.page';
       }
     ])
   ],
-  declarations: [HomePage]
+  declarations: [HomePage,EventComponent]
 })
 export class HomePageModule {}
