0e95430337acc8972f87e7911f01e95660bf512c
Abdelouahid-eddaoudi <abdelouahid.eddaoudi@gmail.com>
2020-05-19 14:16:57 +0000

    Add Popup to chose avatar from existing avatar

---
+++ src/app/avatar-dialog/avatar-dialog.component.ts
@@ -0,0 +1,34 @@
+import { Component, OnInit } from '@angular/core';
+import {ModalController} from '@ionic/angular';
+
+@Component({
+  selector: 'app-avatar-dialog',
+  templateUrl: './avatar-dialog.component.html',
+  styleUrls: ['./avatar-dialog.component.scss'],
+})
+export class AvatarDialogComponent implements OnInit {
+
+    avatars: Array<string> = new Array<string>();
+    link;
+    constructor(
+        public modalController: ModalController
+    ) { }
+
+    ngOnInit() {
+        this.getAvatar();
+    }
+
+    getAvatar() {
+        for ( let i = 1 ; i < 22 ; i++) {
+          this.avatars.push('/assets/avatar/avatar' + i + '.png');
+        }
+    }
+
+    dismiss(avatarImg) {
+        // using the injected ModalController this page
+        // can "dismiss" itself and optionally pass back data
+        this.modalController.dismiss({
+            avatar: avatarImg
+        });
+    }
+}
